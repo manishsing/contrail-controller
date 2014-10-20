6a60f6de44d3cb57f9687aa8b425b3e81124f375
Praveen K V <kvpraveen@juniper.net>
2014-10-20 11:52:58 +0530

    Support adding l2 routes from OVS clients
    Move ovs_peer.cc and .h to ovsdb_client/ directory

--- src/vnsw/agent/physical_devices/ovs_tor_agent/ovsdb_client/ovsdb_route_peer.h
+++ src/vnsw/agent/physical_devices/ovs_tor_agent/ovsdb_client/ovsdb_route_peer.h
@@ -1,22 +1,27 @@
 /*
  * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
  */
-#ifndef SRC_VNSW_AGENT_PHYSICAL_DEVICES_OVS_TOR_AGENT_OVS_PEER_H_
-#define SRC_VNSW_AGENT_PHYSICAL_DEVICES_OVS_TOR_AGENT_OVS_PEER_H_
+#ifndef SRC_VNSW_AGENT_PHYSICAL_DEVICES_OVS_TOR_AGENT_OVSDB_CLIENT_OVS_PEER_H_
+#define SRC_VNSW_AGENT_PHYSICAL_DEVICES_OVS_TOR_AGENT_OVSDB_CLIENT_OVS_PEER_H_
 
 #include <cmn/agent_cmn.h>
 #include <oper/peer.h>
 
+class OvsPeerManager;
+
 // Peer for routes added by OVS
 class OvsPeer : public Peer {
  public:
-    OvsPeer(const IpAddress &peer_ip, uint64_t gen_id);
+    OvsPeer(const IpAddress &peer_ip, uint64_t gen_id, OvsPeerManager *manager);
     virtual ~OvsPeer();
 
     bool Compare(const Peer *rhs) const;
+    bool AddOvsRoute(const boost::uuids::uuid &vn_uuid, const MacAddress &mac,
+                     Ip4Address &tor_ip);
  private:
     IpAddress peer_ip_;
     uint64_t gen_id_;
+    OvsPeerManager *peer_manager_;
     DISALLOW_COPY_AND_ASSIGN(OvsPeer);
 };
 
@@ -29,16 +34,18 @@ class OvsPeerManager {
     };
     typedef std::set<OvsPeer *, cmp> OvsPeerTable;
 
-    OvsPeerManager();
+    OvsPeerManager(Agent *agent);
     virtual ~OvsPeerManager();
 
     OvsPeer *Allocate(const IpAddress &peer_ip);
     void Free(OvsPeer *peer);
+    Agent *agent() const;
 
  private:
     uint64_t gen_id_;
+    Agent *agent_;
     OvsPeerTable table_;
     DISALLOW_COPY_AND_ASSIGN(OvsPeerManager);
 };
 
-#endif  // SRC_VNSW_AGENT_PHYSICAL_DEVICES_OVS_TOR_AGENT_OVS_PEER_H_
+#endif  // SRC_VNSW_AGENT_PHYSICAL_DEVICES_OVS_TOR_AGENT_OVSDB_CLIENT_OVS_PEER_H_
