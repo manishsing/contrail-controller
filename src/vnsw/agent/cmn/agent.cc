96737580c9fe3e6a7ac25fe815541a6f9aa021ca
Praveen K V <pkv@contrailsystems.com>
2014-10-22 00:16:25 +0530

    Merge with master

--- src/vnsw/agent/cmn/agent.cc
+++ src/vnsw/agent/cmn/agent.cc
@@ -502,14 +502,6 @@ DomainConfig *Agent::domain_config_table() const {
     return oper_db_->domain_config_table();
 }
 
-PhysicalDeviceManager *Agent::device_manager() const {
-    return device_manager_;
-}
-
-void Agent::set_device_manager(PhysicalDeviceManager *dev_mgmt) {
-    device_manager_ = dev_mgmt;
-}
-
 bool Agent::isVmwareMode() const {
     return params_->isVmwareMode();
 }
@@ -519,4 +511,12 @@ bool Agent::isVmwareVcenterMode() const {
         return false;
 
     return params_->isVmwareVcenterMode();
+}
+
+PhysicalDeviceManager *Agent::device_manager() const {
+    return device_manager_;
+}
+
+void Agent::set_device_manager(PhysicalDeviceManager *dev_mgmt) {
+    device_manager_ = dev_mgmt;
 }
