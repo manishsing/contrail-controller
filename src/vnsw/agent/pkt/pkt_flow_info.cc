152add88ff8a99c59333086227479d888aff0aad
Praveen KV <kvpraveen@juniper.net>
2014-11-12 21:12:06 +0530

    Fixes for agent to work with un-numbered vhost0
    There are two issues in case when vhost0 is unnumbered in host-os.
    
    1. When vhost0 interface is un-numbered in host-os, packets from host-os
       addressed to metadata-ip for VM will use an unknown-ip to vrouter. This
       will result in packets being dropped since source-ip is not-known.
    2. When agent runs http-proxy as part of metadata services, packets from
       VM for metadat-service must be NAT-ed to an IP address on host-os.
       When vhost0 is unnumbered, VRouter must use one of the IP address in
       host-os.
    
    Agent is modified to support following,
    Agent takes the set of IP address on host-os as vhost_services_ip.
    Agent will use one of the vhost_services_ip for NAT-ing of packet from
    VM to metadata service

--- src/vnsw/agent/pkt/pkt_flow_info.cc
+++ src/vnsw/agent/pkt/pkt_flow_info.cc
@@ -231,7 +231,15 @@ static bool NhDecode(const NextHop *nh, const PktInfo *pkt, PktFlowInfo *info,
 
     case NextHop::ARP: {
         const ArpNH *arp_nh = static_cast<const ArpNH *>(nh);
-        out->nh_ = arp_nh->GetInterface()->flow_key_nh()->id();
+        if (in->intf_->type() == Interface::VM_INTERFACE) {
+            const VmInterface *vm_intf =
+                static_cast<const VmInterface *>(in->intf_);
+            if (vm_intf->sub_type() == VmInterface::VCPE) {
+                out->nh_ = arp_nh->id();
+            } else {
+                out->nh_ = arp_nh->GetInterface()->flow_key_nh()->id();
+            }
+        }
         out->intf_ = arp_nh->GetInterface();
         break;
     }
@@ -528,6 +536,9 @@ void PktFlowInfo::LinkLocalServiceFromVm(const PktInfo *pkt, PktControlInfo *in,
         // the packet not coming to vrouter for reverse NAT.
         // Destination would be local host (FindLinkLocalService returns this)
         nat_ip_saddr = vm_port->mdata_ip_addr();
+        // Services such as metadata will run on vhost_services_ip. Set nat
+        // address to vhost_services_ip
+        nat_server = Agent::GetInstance()->vhost_services_ip();
         nat_sport = pkt->sport;
     } else {
         nat_ip_saddr = Agent::GetInstance()->router_id();
@@ -569,8 +580,8 @@ void PktFlowInfo::LinkLocalServiceFromHost(const PktInfo *pkt, PktControlInfo *i
         return;
     }
 
-    if ((pkt->ip_daddr.to_v4().to_ulong() != vm_port->mdata_ip_addr().to_ulong()) ||
-        (pkt->ip_saddr.to_v4().to_ulong() != Agent::GetInstance()->router_id().to_ulong())) {
+    // Check if packet is destined to metadata of interface
+    if (pkt->ip_daddr.to_v4() != vm_port->mdata_ip_addr()) {
         // Force implicit deny
         in->rt_ = NULL;
         out->rt_ = NULL;
